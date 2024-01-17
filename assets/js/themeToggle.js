e4cc8e8a0552e6756b5915f29b978d4f39debeb9
hercjay <iamhercjay@gmail.com>
2024-01-17 11:15:26 +0100

    updated theme toggle icons responsiveness

--- assets/js/themeToggle.js
+++ assets/js/themeToggle.js
@@ -8,10 +8,14 @@
         themeToggleLabel.addEventListener('click', function () {
             if (themeStylesheet.href.endsWith('assets/css/light.css')) {
                 themeStylesheet.href = 'assets/css/dark.css';
-                themeToggleModeText.textContent = 'Light';
+                // themeToggleModeText.textContent = 'Light';
+                themeToggleLabel.style.color = '#fff';
             } else {
                 themeStylesheet.href = 'assets/css/light.css';
-                themeToggleModeText.textContent = 'Dark';
+                // themeToggleModeText.textContent = 'Dark';
+                themeToggle.classList.remove('text-light');
+                themeToggleModeText.classList.remove('text-light');
+                themeToggleLabel.style.color = '#ffb833';
             }
         });
     });
