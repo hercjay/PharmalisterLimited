482c26c83c70e8e542412bda8d7262d55a643257
hercjay <iamhercjay@gmail.com>
2024-03-17 08:27:04 +0100

    make toggle icon colour change to stay visible on mobile display

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,6 +113,7 @@
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
+    this.classList.toggle('text-dark')
     this.classList.toggle('bi-x')
   })
 
